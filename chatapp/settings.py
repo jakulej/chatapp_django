7692505524bff4a1859f7f8fa0bd41f3e7d4b54e
jakulej <j.kulej01@gmail.com>
2025-04-04 15:06:20 +0200

    Added redis for caching sessions

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -82,6 +82,12 @@ CHANNEL_LAYERS = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis:redis:6379",
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
