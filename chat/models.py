a166ef71fcb58f07fd889b30c97c340a0d84e195
jakulej <j.kulej01@gmail.com>
2025-04-07 20:59:21 +0200

    Fixed wrong rellation betwen Room and messages

--- chat/models.py
+++ chat/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Room(models.Model):
+    users = models.ManyToManyField(User, related_name='rooms')   
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'{self.sender}: {self.content}'
-
-class Room(models.Model):
-    users = models.ManyToManyField(User)   
-    messages = models.ManyToManyField(Message)
