5dd0f9654f410fbbc8da3dbd25d54ddf9ad5b14d
jakulej <j.kulej01@gmail.com>
2025-04-11 17:41:48 +0200

    Messages now are saved to db on send

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,11 @@
 # chat/consumers.py
 import json
+import datetime
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer 
-from .models import Room
+from .models import Room, Message
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -26,6 +28,8 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        print(message)
+        self.save_message(message)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name, {
@@ -45,4 +49,10 @@ class ChatConsumer(WebsocketConsumer):
             "message": message,
             "user" : user
             }))
+    
+    def save_message(self, content):
+        time = datetime.datetime.now()
+        room_obj = Room.objects.get(id=self.room_name)
+        message = Message(sender=self.user, room=room_obj, content=content, timestamp=time)
+        message.save()
 
