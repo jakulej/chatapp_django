e4cc8c4cd4b4444f4ee153b30ccdedea3948b995
jakulej <j.kulej01@gmail.com>
2025-04-24 20:02:18 +0200

    Create view find friend

--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+def find_friend(request, query):
+    User = get_user_model()
+    result = User.objects.filter(username__icontains=query)[:5]
+    return render(request, "friends/add-friend.html",{"friend_result":result})
