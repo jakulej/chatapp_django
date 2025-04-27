73c7406198e808ac3efe6ccbdbf82a73209cb6e9
jakulej <j.kulej01@gmail.com>
2025-04-27 20:46:08 +0200

    Fixed error where query results mismatched variable names

--- friends/views.py
+++ friends/views.py
@@ -5,4 +5,5 @@ from django.contrib.auth import get_user_model
 def find_friend(request, query):
     User = get_user_model()
     result = User.objects.filter(username__icontains=query)[:5]
-    return render(request, "friends/add-friend.html",{"friend_result":result})
+    print(result)
+    return render(request, "friends/add-friend.html",{"friends_results":result})
