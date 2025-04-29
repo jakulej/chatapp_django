fcb23fed6dbd96791966a3fa6a8c5105ce84dcba
jakulej <j.kulej01@gmail.com>
2025-04-29 16:31:59 +0200

    Added login_required on some views

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from .models import Message, Room
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, "chat/index.html")
 
+@login_required
 def room(request, room_id):
     room = Room.objects.get(id=room_id)
     latest_messages = room.messages.order_by("timestamp")[:10]
