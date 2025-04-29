923af120736c0d8dd3ea441262b270c9d7eeb4b6
jakulej <j.kulej01@gmail.com>
2025-04-29 16:31:18 +0200

    Added view add_friend

--- friends/urls.py
+++ friends/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
         path("find/<str:query>", views.find_friend, name="find-friend"),
+        path("add/<str:username>", views.add_friend, name="add-friend"),
+
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 # Create your views here.
+@login_required
 def find_friend(request, query):
     User = get_user_model()
-    result = User.objects.filter(username__icontains=query)[:5]
+    user = request.user
+
+    result = User.objects.filter(username__icontains=query).exclude(username=user.username)[:5]
     print(result)
     return render(request, "friends/add-friend.html",{"friends_results":result})
+
+def add_friend(request, username):
+    if request.method == 'POST':
+        User = get_user_model()
+        user = request.user
+        friend = User.objects.get(username=username)
+
+        user.friends.add(friend)
+        user.save()
+
+    return redirect("/chat/index/")
