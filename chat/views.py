9b863780f4d286442c2b148aca06940b19a53d27
jakulej <j.kulej01@gmail.com>
2025-08-11 19:33:13 +0200

    Created template function

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Message, Room
 from django.contrib.auth.decorators import login_required
-
+from .room_managment import create_room
+from django.http import HttpResponse
 # Create your views here.
 @login_required
 def index(request):
@@ -12,3 +13,11 @@ def room(request, room_id):
     room = Room.objects.get(id=room_id)
     latest_messages = room.messages.order_by("timestamp")[:10]
     return render(request, "chat/room.html", {"room_id": room_id, "latest_messages": latest_messages})
+
+@login_required
+def create_room(request):
+    if request.method == 'POST':
+        user = request.user
+        print(request.body.users)
+    return HttpResponse(status=200)
+    
