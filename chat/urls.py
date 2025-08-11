f5183a494dda80f2c8aa0d396d1547b449811b8b
jakulej <j.kulej01@gmail.com>
2025-08-11 19:33:00 +0200

    Created Url

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:room_id>/", views.room, name="room"),
+    path("createRoom/", views.create_room, name="createRoom"),
 ]
