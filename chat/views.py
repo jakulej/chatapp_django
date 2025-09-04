fda2a148fd8e7e785fcaabd1694979c2ffa44c08
jakulej <j.kulej01@gmail.com>
2025-09-04 14:05:49 +0200

    Format code

--- chat/views.py
+++ chat/views.py
@@ -6,17 +6,27 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 import json
 # Create your views here.
+
+
 @login_required
 def index(request, room_id):
     room = Room.objects.get(id=room_id)
     latest_messages = room.messages.order_by("timestamp")[:10]
-    return render(request, "chat/index.html", {"room_id": room_id, "latest_messages": latest_messages})
+    return render(request, "chat/index.html", {
+        "room_id": room_id,
+        "latest_messages": latest_messages
+        })
+
 
 @login_required
 def room(request, room_id):
     room = Room.objects.get(id=room_id)
     latest_messages = room.messages.order_by("timestamp")[:10]
-    return render(request, "chat/room.html", {"room_id": room_id, "latest_messages": latest_messages})
+    return render(request, "chat/room.html", {
+        "room_id": room_id,
+        "latest_messages": latest_messages
+        })
+
 
 @login_required
 def create_room(request):
@@ -26,8 +36,7 @@ def create_room(request):
         users = json.loads(request.body.decode('utf-8'))['users']
         users = list(map(lambda user: User.objects.get(username=user), users))
         users.append(room_creator)
-        
-        create_room_obj(users,"Group")
+
+        create_room_obj(users, "Group")
         print(users)
     return HttpResponse(status=200)
-    
